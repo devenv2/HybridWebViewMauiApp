2f67a14dab85dd4c70d3c44d16d6dc5956339a99
yusuf <yusuf@DESKTOP-Q77MT1Q>
2025-05-10 10:17:38 +0300

    Add project files.

---
+++ HybridWebViewMauiApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace HybridWebViewMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void OnSendMessageButtonClicked(object sender, EventArgs e)
+        {
+            hybridWebView.SendRawMessage($"Hello from C#!");
+        }
+
+        private async void OnHybridWebViewRawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
+        {
+            await DisplayAlert("Raw Message Received", e.Message, "OK");
+        }
+
+    }
+
+}
